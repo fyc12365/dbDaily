620c595d793a2161b1d636aa3e0f9b341ed6092b
fyc12365 <51687826+fyc12365@users.noreply.github.com>
2019-07-17 08:32:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import pygame
+import time
+from pygame.locals import *
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((800,600),0,32)
+pygame.display.set_caption('有点儿正常的大冒险')
+icon = pygame.image.load('Data/image/icon.png')
+pygame.display.set_icon(icon)
+bg = pygame.image.load('Data/image/bg.png')
+button_start = pygame.image.load('Data/image/start.png')
+mouse_replace = pygame.image.load('Data/image/mouse.png')
+clock = pygame.time.Clock()
+pygame.mouse.set_visible(False)
+
+def main_menu():
+    pygame.mixer.music.load('Data/music/menu_bgm.mp3')
+    pygame.mixer.music.play(1,0)
+    buloon_1 = 0
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+            elif event.type == MOUSEBUTTONDOWN and 275<=event.pos[0]<=525 and 400<=event.pos[1]<=500 :
+                buloon_1 = 1
+                pygame.mixer.music.stop()
+                break
+        if buloon_1 == 1:
+            break
+        screen.blit(bg,(0,0))
+        screen.blit(button_start,(275,400))
+        x, y = pygame.mouse.get_pos()
+        screen.blit(mouse_replace,(x,y))
+        pygame.display.update()
+
+def scene_1():
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+        screen.blit(bg,(0,0))
+        x,y = pygame.mouse.get_pos()
+        screen.blit(mouse_replace,(x,y))
+        pygame.display.update()
+
+main_menu()
+scene_1()
+
+os.system('pause')
