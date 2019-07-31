d17f70e88f529b6b99a8c8ad1b5861dccaf27401
fyc12365 <51687826+fyc12365@users.noreply.github.com>
2019-07-31 17:22:46 +0800

    生成第一个尝鲜版

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import pygame
 import time
 from pygame.locals import *
+from sys import exit
 
 pygame.init()
 pygame.mixer.init()
@@ -11,7 +12,6 @@ icon = pygame.image.load('Data/image/icon.png')
 pygame.display.set_icon(icon)
 bg = pygame.image.load('Data/image/bg.png')
 button_start = pygame.image.load('Data/image/start.png')
-scene_1_bg = pygame.image.load('Data/image/scene_1_bg.png')
 block_ = pygame.image.load('Data/image/block.png')
 error_1 = pygame.image.load('Data/image/error_1.jpg')
 mouse_replace = pygame.image.load('Data/image/mouse.png')
@@ -44,6 +44,8 @@ text_1 = textFont_1.render('好好学一学数学！智商-10',True,(0,0,0))
 text_money1 = textFont_2.render('你有：$1.000,000,000,000',True,(0,0,0))
 text_money2 = textFont_2.render('你有：$1,000,000,000,000',True,(0,0,0))
 
+bg = pygame.image.load('Data/image/scene_1_bg.png')
+
 health = 100
 strength = 10
 iq = 250
@@ -55,7 +57,6 @@ iq_warn = False
 beauty_warn = False
 san_warn = False
 textFont_character = pygame.font.Font('C:\Windows\Fonts\simhei.ttf',18)
-
 buloon_1 = False
 buloon_2 = False
 pygame.mixer.music.load('Data/music/scene_1_bgm.ogg')
@@ -69,13 +70,14 @@ while True:
             try:
                 if 136<=event.pos[0]<=160 and 254<=event.pos[1]<=278:
                     timer_0 = time.time()
+                    iq = iq - 10
                     while True:
                         for event in pygame.event.get():
                             if event.type == QUIT:
                                 exit()
                         if time.time() >= timer_0+5:
                             break
-                        screen.blit(scene_1_bg,(0,0))
+                        screen.blit(bg,(0,0))
                         screen.blit(block_,(0,0))
                         screen.blit(error_1,(260,210))
                         screen.blit(text_1,(120,180))
@@ -84,13 +86,14 @@ while True:
                         pygame.display.update()
                 if 136<=event.pos[0]<=160 and 317<=event.pos[1]<=341:
                     timer_0 = time.time()
+                    iq = iq - 10
                     while True:
                         for event in pygame.event.get():
                             if event.type == QUIT:
                                 exit()
                         if time.time() >= timer_0+5:
                             break
-                        screen.blit(scene_1_bg,(0,0))
+                        screen.blit(bg,(0,0))
                         screen.blit(block_,(0,0))
                         screen.blit(error_1,(260,210))
                         screen.blit(text_1,(120,180))
@@ -98,6 +101,7 @@ while True:
                         screen.blit(mouse_replace,(x,y))
                         pygame.display.update()
                 if 136<=event.pos[0]<=160 and 380<=event.pos[1]<=404:
+                    iq = iq - 10
                     timer_0 = time.time()
                     while True:
                         for event in pygame.event.get():
@@ -105,7 +109,7 @@ while True:
                                 exit()
                         if time.time() >= timer_0+5:
                             break
-                        screen.blit(scene_1_bg,(0,0))
+                        screen.blit(bg,(0,0))
                         screen.blit(block_,(0,0))
                         screen.blit(error_1,(260,210))
                         screen.blit(text_1,(120,180))
@@ -117,12 +121,25 @@ while True:
                 if 136<=event.pos[0]<=160 and 443<=event.pos[1]<=467:
                     if buloon_2 == True:
                         buloon_1 = True
-                    break
+                        break
+                    timer_0 = time.time()
+                    iq = iq - 10
+                    while True:
+                        for event in pygame.event.get():
+                            if event.type == QUIT:
+                                exit()
+                        if time.time() >= timer_0+5:
+                            break
+                        screen.blit(bg,(0,0))
+                        screen.blit(block_,(0,0))
+                        screen.blit(error_1,(260,210))
+                        screen.blit(text_1,(120,180))
+                        x,y = pygame.mouse.get_pos()
+                        screen.blit(mouse_replace,(x,y))
+                        pygame.display.update()
             except:
                 pass
-    screen.blit(scene_1_bg,(0,0))
-    x,y = pygame.mouse.get_pos()
-    screen.blit(mouse_replace,(x,y))
+    screen.blit(bg,(0,0))
     if buloon_2 == False:
         screen.blit(text_money1,(350,550))
     else:
@@ -130,34 +147,64 @@ while True:
     if buloon_1 == True:
         pygame.mixer.music.stop()
         break
+    x,y = pygame.mouse.get_pos()
+    screen.blit(mouse_replace,(x,y))
     pygame.display.update()
 
 def update_health():
+    global health
     if health > 100:
         health = 100
-    text_health = textFont_character.render('生命：{0}/100'.format(health),True,(0,0,0))
 def update_strength():
+    global strength
     if strength >100:
         strength = 100
-    text_strength = textFont_character.render('力量：{0}/100'.format(strength),True,(0,0,0))
 def update_iq():
+    global iq
+    global iq_warn
     if iq > 250:
         iq = 250
         iq_warn = True
-    text_iq = textFont_character.render('智商：{0}/250'.format(iq),True,(0,0,0))
 def update_beauty():
+    global beauty
+    global beauty_warn
     if beauty > 100:
         beauty = 100
         beauty_warn = True
-    text_beauty = textFont_character.render('颜值：{0}/100'.format(beauty),True,(0,0,0))
 def update_san():
+    global san
+    global san_warn
     if san > 100:
         san = 100
         san_warn = True
-    test_san = textFont_character.render('精神：{0}/100'.format(san),True,(0,0,0))
 
 update_health()
 update_strength()
 update_iq()
 update_beauty()
 update_san()
+
+text_health = textFont_character.render('生命：{0}/100'.format(health),True,(0,0,0))
+text_strength = textFont_character.render('力量：{0}/100'.format(strength),True,(0,0,0))
+text_san = textFont_character.render('精神：{0}/100'.format(san),True,(0,0,0))
+text_iq = textFont_character.render('智商：{0}/250'.format(iq),True,(0,0,0))
+text_beauty = textFont_character.render('颜值：{0}/100'.format(beauty),True,(0,0,0))
+bg = pygame.image.load('Data/image/main_bg.png')
+
+text_test = textFont_1.render('未完待续...',True,(0,0,0))
+
+while True:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            exit()
+    screen.blit(bg,(0,0))
+    screen.blit(text_health,(10,10))
+    screen.blit(text_strength,(10,40))
+    screen.blit(text_iq,(10,70))
+    screen.blit(text_beauty,(10,100))
+    screen.blit(text_san,(10,130))
+    screen.blit(text_test,(300,250))
+    x,y = pygame.mouse.get_pos()
+    screen.blit(mouse_replace,(x,y))
+    pygame.display.update()
