43de420554a4cf623f8176af87aec52ff638957f
fyc12365 <fyc12365@users.noreply.github.com>
2019-09-04 22:33:12 +0800

    Update dbDaily.py

--- dbDaily.py
+++ dbDaily.py
@@ -4,4 +4,17 @@ import time
 from pygame.locals import *
 
 class character(object):
-    def __init__(self,name,health,strength,iq,beauty,san,backpack={}):
+    def __init__(self,name,health,strength,iq,beauty,san):
+        self.name = name
+        self.health = health
+        self.strength = strength
+        self.iq = iq
+        self.beauty = beauty
+        self.san = san
+        self.backpack = {}
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((800,600),0,32)
+pygame.display.set_caption("有点儿正常的大冒险")
+pygame.display.set_icon(pygame.image.load("Data/image/icon.png"))
