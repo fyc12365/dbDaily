7baecfc66e92ebd7ee03aed0321953d3448396b8
fyc12365 <fyc12365@users.noreply.github.com>
2019-09-06 20:00:03 +0800

    Update dbDaily.py

--- dbDaily.py
+++ dbDaily.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 
 #定义角色类
 class character(object):
-    def __init__(self,name,health,strength,iq,beauty,san):
+    def __init__(self,name,health,strength,iq,beauty,san,pos_x=0,pos_y=0):
         self.name = name
         self.health = health
         self.strength = strength
@@ -13,14 +13,20 @@ class character(object):
         self.beauty = beauty
         self.san = san
         self.backpack = {}
-    def move_left():
-        pass
-    def move_right():
-        pass
-    def move_up():
-        pass
-    def move_down():
-        pass
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+    def move_left(self):
+        self.pos_x = self.pos_x - 1
+        print(self.pos_x,self.pos_y)
+    def move_right(self):
+        self.pos_x = self.pos_x + 1
+        print(self.pos_x,self.pos_y)
+    def move_up(self):
+        self.pos_y = self.pos_y - 1
+        print(self.pos_x,self.pos_y)
+    def move_down(self):
+        self.pos_y = self.pos_y + 1
+        print(self.pos_x,self.pos_y)
 
 #初始化pygame及其混响（声音）
 pygame.init()
@@ -32,3 +38,23 @@ pygame.display.set_icon(pygame.image.load("Data/image/icon.png"))
 #隐藏鼠标并加载替代图片
 mouse_replace = pygame.image.load("Data/image/mouse.png")
 pygame.mouse.set_visible(False)
+#定义帧控制
+clock = pygame.time.Clock()
+#实例化角色类，角色名为LGD
+LGD = character("李狗蛋",100,10,250,10,100)
+
+while True:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                LGD.move_up()
+            elif event.key == K_DOWN:
+                LGD.move_down()
+            elif event.key == K_LEFT:
+                LGD.move_left()
+            elif event.key == K_RIGHT:
+                LGD.move_right()
+    pygame.display.update()
